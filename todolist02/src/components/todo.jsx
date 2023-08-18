2fbaaafdd5bb70d91a2421699aca2be68733d501
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-18 13:25:20 +0200

    update

--- todolist02/src/components/todo.jsx
+++ todolist02/src/components/todo.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import style from "../style/todo.module.scss";
 
+
 export const Todo = () => {
-  const [todoArray, setTodoArray] = useState([]);
+
+    const [todoArray, setToDoArray] = useState([''])
+
+//   const [todoArray, setTodoArray] = useState([]);
+
+  const [text, setText] = useState('')
+
 
   //New const 
   const [inputText, setInputText] = useState("");
 
   const addToArray = () => {
-    let clone = [...todoArray];
-    clone.push("new item");
-    setTodoArray(clone);
-  };
+        let clone = [...todoArray]
+        clone.push(text)
+        console.log(clone);
+        setToDoArray(clone)
+    }
 
 //   const deleteArray = () => {
 //     let delete = [...inputText];
@@ -24,10 +32,12 @@ export const Todo = () => {
     <section className={style.todoStyle}>
 
       <h3>Add text</h3>
-      <input type="text" />
-      <button onClick={addToArray}>Add the Text</button>
+      {/* <input placeholder={"sæt her"} value={text} onChange={(event) => inputText(event.target.value)} /> */}
+      <input placeholder={"Sæt ind her"} value={text} onChange={(event) => setText(event.target.value)} />
       <div>
         {todoArray && todoArray.map((item, index) => <p key={index}>{item}</p>)}
+        <button onClick={addToArray}>Tilføj</button>
+
       </div>
 
     </section>
